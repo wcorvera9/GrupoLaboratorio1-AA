1a028d376b16620c1d21c6496048c7d1384db119
AMPablo4532 <00149726@uca.edu.sv>
2026-09-10 10:07:08 -0600

    :white_check_mark: feat::add funcion de consumir combustible incluyendo pruebas en su funcionamiento

--- Main.cc
+++ Main.cc
@@ -1,6 +1,44 @@
 #include <iostream>
 
+// Declaracion de funciones
+// Función para consumir combustible (paso por puntero)
+void ConsumirCombustible(double *combustible, double litros);
+
 int main() {
-    
+    // Combustible inicial
+    double combustible = 50.0;
+
+    // Demostración 3: Consumo por puntero (Casos de prueba)
+    ConsumirCombustible(&combustible, -5.0);  // Error: Cantidad negativa
+    ConsumirCombustible(&combustible, 90.0);  // Error: Excede el combustible disponible (80 - 90 < 0)
+    ConsumirCombustible(&combustible, 45.0);  // Éxito: Disminuye a 35 L
     return 0;
-}
+};
+
+
+// Consumo de combustible (paso por puntero)
+void ConsumirCombustible(double *combustible, double litros)
+{
+    std::cout << "\n--- Intentando consumir " << litros << " L ---" << std::endl;
+    
+    if (combustible == nullptr) {
+        std::cout << "[ERROR] Puntero no valido." << std::endl;
+        return;
+    }
+    if (litros <= 0)
+    {
+        std::cout << "Error: La cantidad a consumir debe ser mayor que 0." << std::endl;
+        return;
+    }
+
+    if (*combustible - litros < 0)
+    {
+        std::cout << "Error: No hay suficiente combustible para consumir." << std::endl;
+        std::cout << "Combustible disponible: " << *combustible << " litros" << std::endl;
+        return;
+    }
+
+    *combustible -= litros;
+    std::cout << "Consumo exitoso. Se consumieron " << litros << " litros." << std::endl;
+    std::cout << "Nivel despues de la operacion: " << *combustible << " L" << std::endl;
+};
